861c7595b35361eb4131296697de99372ad2902d
tanvirch0750 <tanvir0750@yahoo.com>
2022-07-15 20:26:05 +0600

    add react router dom and implemented route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
+import List from './pages/list/List';
+import Login from './pages/login/Login';
+import New from './pages/new/New';
+import Single from './pages/single/Single';
 
 function App() {
   return (
     <>
-      <Home />
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />}></Route>
+          <Route path="login" element={<Login />}></Route>
+          <Route path="users">
+            <Route index element={<List />}></Route>
+            <Route path=":userId" element={<Single />}></Route>
+            <Route path="new" element={<New />}></Route>
+          </Route>
+          <Route path="products">
+            <Route index element={<List />}></Route>
+            <Route path=":productId" element={<Single />}></Route>
+            <Route path="new" element={<New />}></Route>
+          </Route>
+        </Route>
+      </Routes>
     </>
   );
 }
