f0ead1fae332f0de44a2fac9103d88518e3ae65b
tanvirch0750 <tanvir0750@yahoo.com>
2022-07-15 23:22:26 +0600

    completed the navbar section

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,54 @@
 import './Navbar.scss';
 
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <header className="navbar">
+      <nav className="wrapper">
+        <div className="search">
+          <input type="text" placeholder="Search" />
+          <SearchOutlinedIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon />
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon />
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon />
+            <div className="counter">3</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon />
+          </div>
+          <div className="item">
+            <img
+              src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              alt="avatar"
+              className="avatar"
+            />
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
